df55beab9738df4a96c74d9da1717455467b742c
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2022-12-17 23:03:23 -0800

    added basic template

--- template_project/project_name/settings.py
+++ template_project/project_name/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django_extensions",
     "debug_toolbar",
+    # our apps
+    "core",
 ]
 
 MIDDLEWARE = [
--- template_project/project_name/urls.py
+++ template_project/project_name/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
+    # our applications
+    path("", include('core.urls')),
 ]
